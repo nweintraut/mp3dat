1b5bc489223d7ddd6f9e97dfd2064f5f3e51a32c
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-24 17:35:17 -0400

    Server example working.

---
+++ stdin_stream.js
@@ -0,0 +1,17 @@
+// to use try :
+// cat ../test/test.mp3 | node stdin_stream.js 82989
+// the argument (82969) is the size in bytes of the mp3
+
+if(!process.argv[2]) {
+    process.stderr.write('\nNeed mp3 size in bytes\n\n');
+    process.exit();
+}
+var mp3dat = require('./lib');
+process.stdin.resume();
+mp3dat.statStream({stream : process.stdin, size: process.argv[2]}, function(err, stats) {
+    if (err) {console.log(err);}
+    console.log(stats);
+});
+
+
+
--- test/index.js
+++ test/index.js
@@ -7,8 +7,6 @@ var mp3dat2 = mp3dat.spawnInstance();
 var testFile = 'test/test.mp3';
 
 
-
-
 assert(mp3dat, 'mp3dat failed to load');
 assert(mp3dat.stat, 'there should be a stat method');
 assert(mp3dat.stat instanceof Function, 'stat should be a Function');
